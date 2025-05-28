a546b6c2872a23abf8ee5929cad2f60d8be51c75
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-05-28 16:03:14 +0100

    fix: improve MongoDB connection logging for better debugging

--- backend/src/config/db/db.ts
+++ backend/src/config/db/db.ts
@@ -4,7 +4,10 @@ import { DB_NAME } from "../../../dbname.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(connectionInstance);
+        console.log(` \n MongoDB connected ! DB host: ${connectionInstance.connection.host}`);
+        console.log("connected to MongoDB successfully");
+        console.log("CONFIDENCE TOO BAD");
+        
         
     } catch (error) {
         console.error("MongoDB connection error", error);
